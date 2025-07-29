d003f585db7eafceeb407c102f84e5e530ad5273
Nicolas <nicolasdifabio92@gmail.com>
2025-07-28 21:05:25 -0300

    se modifican algunos Dtos, para que sean Partial

--- src/features/cart/application/dtos/CartDetailViewDto.ts
+++ src/features/cart/application/dtos/CartDetailViewDto.ts
@@ -1,13 +1,8 @@
+import { CreateProductDto } from "../../../products/application/dtos/CreateProductDto";
+
 export interface CartDetailViewDto {
   id: string;
-  products: Array<{
-    id: string;
-    title: string;
-    price: number;
-    quantity: number;
-    category: string;
-    thumbnails: string[];
-  }>;
+  products: Partial<CreateProductDto>[];
   totalPrice: number;
   createdAt: Date;
   updatedAt: Date;
--- src/features/products/application/dtos/UpdateProductDto.ts
+++ src/features/products/application/dtos/UpdateProductDto.ts
@@ -1,10 +1,14 @@
-export class UpdateProductDto {
-  title?: string;
-  description?: string;
-  code?: string;
-  price?: number;
-  status?: boolean;
-  stock?: number;
-  category?: string;
-  thumbnails?: string[];
-}
+import { CreateProductDto } from "./CreateProductDto";
+
+// export class UpdateProductDto {
+//   title?: string;
+//   description?: string;
+//   code?: string;
+//   price?: number;
+//   status?: boolean;
+//   stock?: number;
+//   category?: string;
+//   thumbnails?: string[];
+// }
+
+export type UpdateProductDto = Partial<CreateProductDto>;
